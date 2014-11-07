30ec4d1d014dd3a2f28c753a8ca8d684367cceb6
Tamas Hegedus <sorgloomer@gmail.com>
2014-11-07 23:16:10 +0100

    reformat code and made exports devenv friendly

--- nano-amd.js
+++ nano-amd.js
@@ -1,27 +1,33 @@
-(function(context, undef) {
-	var modules = {};
-	function defined(name) {
-		return Object.prototype.hasOwnProperty.call(modules, name);
-	}
+(function() {
+    var modules = {};
+
+    function defined(name) {
+        return Object.prototype.hasOwnProperty.call(modules, name);
+    }
+
     function fail(msg) {
         throw new Error(msg);
     }
-	function define(name, deps, factory) {
-		if (defined(name)) fail("Multiple modules with name " + name);
+
+    function define(name, deps, factory) {
+        if (defined(name)) fail("Multiple modules with name " + name);
         var state = 0, module = null;
-		modules[name] = function() {
+        modules[name] = function () {
             if (state > 1) return module;
             if (state > 0) fail("Circular dependency including " + name);
             state = 1;
-            module = factory.apply(undef, deps.map(require));
+            module = factory.apply(null, deps.map(require));
             state = 2;
             return module;
         };
-	}
-	function require(name) {
-		if (!defined(name)) fail("Undefined module " + name);
-		return modules[name]();
-	}
-	context.define = define;
-	context.require = require;
-})(window);
+    }
+
+    function require(name) {
+        if (!defined(name)) fail("Undefined module " + name);
+        return modules[name]();
+    }
+
+    var context = window;
+    context.define = define;
+    context.require = require;
+})();
